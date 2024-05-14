d32d384c8311a87e7dadd1e0d53c3cb36ba482be
GencoMattia <gencomattia@hotmail.it>
2024-05-14 18:08:30 +0200

    added if qith a cicle for inside so to push elements in the array with less elements based on the difference variable

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -8,11 +8,14 @@ let difference;
 
 for (let i = 0; i < randomNumber; i++) {
     firstArray.push(Math.floor(Math.random() * 100) + 1)
-
+    // console.log(firstArray)
 }
 for (let i = 0; i < randomNumberBis; i++) {
     secondArray.push(Math.floor(Math.random() * 100) + 1)
+    // console.log(secondArray)
 }
+
+console.log(firstArray, secondArray)
 
 if (firstArray.length === secondArray.length){
     for (let i = 0; i < randomNumber; i++) {
@@ -27,9 +30,15 @@ if (firstArray.length === secondArray.length){
 if (firstArray.length > secondArray.length){
     difference = firstArray.length - secondArray.length
     console.log(difference)
+    for (let k = 0; k < difference; k++){
+        secondArray.push(Math.floor(Math.random() * 100) + 1)
+    }
 } else {
     difference = secondArray.length - firstArray.length
     console.log(difference)
+    for (let k = 0; k < difference; k++){
+        firstArray.push(Math.floor(Math.random() * 100) + 1)
+    }
 }
 
 console.log(firstArray, secondArray)
