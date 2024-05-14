29de034e3fb5bba9c7afbe6c888f31591a738921
GencoMattia <gencomattia@hotmail.it>
2024-05-14 17:19:05 +0200

    added if to print in case of success. added boolean argument so that if the search is not successfull, log negative message

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -3,12 +3,20 @@ fruitList.push ("pesca");
 
 console.log (fruitList);
 
-for (let i = 0; i < fruitList.length; i++) {
+let cocomeroFound;
 
+for (let i = 0; i < fruitList.length; i++) {
     // console.log(fruitList[i]);
+
 
     let checked = fruitList[i];
     if (checked === "cocomero") {
         console.log("Trovato! Devo solo preparare il cocktail.");
+        cocomeroFound = true;
     }
+
+
+}
+if (!cocomeroFound) {
+    console.log("Oh no, devo uscire a comprare il cocomero!")
 }
