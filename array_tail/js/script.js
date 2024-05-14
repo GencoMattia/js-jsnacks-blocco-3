c9d63404240f074e74d2e4aa35534300cfcb85fb
GencoMattia <gencomattia@hotmail.it>
2024-05-14 17:34:57 +0200

    added cicle for to add as many number to the array as the userNumber

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,6 +1,13 @@
-let userNumber = Number.parseInt(prompt("Quanti elementi vuoi che contenga l'array?"), 10)
+let userNumber = Number.parseInt(prompt("Quanti elementi vuoi che contenga l'array?"), 10);
+let userArray = [];
 
-if (userNumber < 1 || userNumber > 20 || isNaN(userNumber)) {
-    userNumber = Math.floor(Math.random() * 20) + 1
-    console.log("Hai inserito un valore non valido. Il numero a te assegnato è " + userNumber)
+if(userNumber < 1 || userNumber > 20 || isNaN(userNumber)) {
+    userNumber = Math.floor(Math.random() * 20) + 1;
+    console.log("Hai inserito un valore non valido. Il numero a te assegnato è " + userNumber);
 }
+
+for(let i = 0; i < userNumber; i++) {
+    userArray.push(Math.floor(Math.random() * 100) + 1)
+}
+
+console.log(userArray)
