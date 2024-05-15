6e408bc9a4cf5d20d8f0b02ba21d0099dbf45763
GencoMattia <gencomattia@hotmail.it>
2024-05-15 12:41:35 +0200

    snack 11 test

---
+++ snack11/js/script.js
@@ -0,0 +1,16 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const array = [];
+let sum = 0;
+
+while(sum < 50){
+    let userNumber = Number.parseInt(prompt("scegli un numero"), 10)
+    if(Number.isNaN(userNumber) && userNumber >= 50){
+        userNumber = Math.round(Math.random() * 10) + 1
+    } else {
+        array.push(userNumber)
+    }
+}
+
+console.log(array)
