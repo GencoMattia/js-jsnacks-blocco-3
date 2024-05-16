10281d474595bf530f4ba2cda9ab2b969a543bec
GencoMattia <gencomattia@hotmail.it>
2024-05-16 11:27:43 +0200

    added arguments

--- snack11/js/script.js
+++ snack11/js/script.js
@@ -1,2 +1,8 @@
 // Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
+function longestWord(firstWord, secondWord) {
+    const firstWord = prompt("Scegli la prima parola")
+    const secondWord = prompt("Scegli la seconda parola")
+
+    console.log(firstWord, secondWord)
+}
