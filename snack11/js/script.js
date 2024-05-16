c303d7b14d378880f1d2dbf5e96fda8956d6a419
GencoMattia <gencomattia@hotmail.it>
2024-05-16 11:41:26 +0200

    function test 1

--- snack11/js/script.js
+++ snack11/js/script.js
@@ -1,8 +1,21 @@
 // Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
-function longestWord(firstWord, secondWord) {
-    const firstWord = prompt("Scegli la prima parola")
-    const secondWord = prompt("Scegli la seconda parola")
+const firstWord = prompt("Scegli la prima parola")
+const secondWord = prompt("Scegli la seconda parola")
 
-    console.log(firstWord, secondWord)
+
+
+function longestWord(firstWord, secondWord) {
+    
+    if(firstWord.length === secondWord.length){
+        const draw = `${firstWord} e ${secondWord} sono lunghi uguali`
+        return draw
+    } else if(firstWord.length > secondWord.length) {
+        return firstWord
+    } else {
+        return secondWord
+    }
+    
 }
+
+console.log(longestWord())
