b748fcceb47858c7068dc5d1ed95555bee401ee7
GencoMattia <gencomattia@hotmail.it>
2024-05-16 12:10:54 +0200

    snack13 base, looking for a way to reverse the number

---
+++ snack13/js/script.js
@@ -0,0 +1,9 @@
+// > Scrivi una funzione che dato un numero inverta le sue cifre: 1234 -> 4321
+
+const userNumber = BigInt(prompt("Scegli un numero"))
+
+function numberReverse(chosenNumber){
+    const reversedNumber = 
+}
+
+console.log(numberReverse(userNumber))
