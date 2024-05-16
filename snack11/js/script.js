649b810eb38a52e473880749b204a0837e3678a0
GencoMattia <gencomattia@hotmail.it>
2024-05-16 11:24:09 +0200

    added snak12 folder with assigment

--- snack11/js/script.js
+++ snack11/js/script.js
@@ -1,17 +1,2 @@
-// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
-// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+// Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
 
-const array = [];
-let sum = 0;
-
-while(sum < 50){
-    let userNumber = Number.parseInt(prompt("scegli un numero"), 10);
-    if(Number.isNaN(userNumber) || userNumber >= 50 || userNumber < 1){
-        userNumber = Math.round(Math.random() * 10) + 1;
-    } else {
-        array.push(userNumber);
-        sum += userNumber;
-    }
-}
-console.log(sum);
-console.log(array);
---
+++ snack12/js/script.js
@@ -0,0 +1,17 @@
+// Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. 
+// Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
+
+const array = [];
+let sum = 0;
+
+while(sum < 50){
+    let userNumber = Number.parseInt(prompt("scegli un numero"), 10);
+    if(Number.isNaN(userNumber) || userNumber >= 50 || userNumber < 1){
+        userNumber = Math.round(Math.random() * 10) + 1;
+    } else {
+        array.push(userNumber);
+        sum += userNumber;
+    }
+}
+console.log(sum);
+console.log(array);
