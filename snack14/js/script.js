78a76ce39600bad0b88d301245087b13569587a4
GencoMattia <gencomattia@hotmail.it>
2024-05-21 12:46:51 +0200

    snack 14: test 3

--- snack14/js/script.js
+++ snack14/js/script.js
@@ -1,10 +1,10 @@
 // ! Chiedi all'utente un numero di secondi da attendere e fai partire un timer da quel momento a zero secondi, interrompendo il timer
 
-let userTImer = Number.parseInt(prompt("Quanti secondi?"));
+let userTImer = Number.parseInt(prompt("Quanti secondi?"), 10);
 
 function timerCountDown(seconds){
-    seconds--;
-    return seconds;
+    seconds = seconds - 1;
+    console.log(seconds)
 };
 
 setInterval(timerCountDown, 1000, userTImer);
