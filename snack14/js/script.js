bdd3522f735027a6b27cd6969e428f6054c2f533
GencoMattia <gencomattia@hotmail.it>
2024-05-22 12:35:50 +0200

    snack 15 added assignment

--- snack14/js/script.js
+++ snack14/js/script.js
@@ -2,9 +2,11 @@
 
 let userTImer = Number.parseInt(prompt("Quanti secondi?"), 10);
 
+let remaining = userTImer
+
 function timerCountDown(seconds){
     seconds = seconds - 1;
     console.log(seconds)
 };
 
-setInterval(timerCountDown, 1000, userTImer);
+setInterval(timerCountDown, 1000, remaining);
