a1a6ee7f0aae0ef079fbcd65835755991a0b9ad3
GencoMattia <gencomattia@hotmail.it>
2024-05-23 12:12:02 +0200

    snackObjects1: added carList array

---
+++ snackObjects1/js/script.js
@@ -0,0 +1,83 @@
+// > Crea un array composto da 15 automobili.
+// > Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+// < Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+// < Infine stampa separatamente i 3 array.
+
+
+const carsList = [
+    { 
+        marca: "Abarth",
+        modello: "500",
+        alimentazione: "benzina"
+    },
+    { 
+        marca: "Abarth",
+        modello: "600",
+        alimentazione: "diesel",
+    },
+    { 
+        marca: "Abarth",
+        modello: "700",
+        alimentazione: "gpl",
+    },
+    { 
+        marca: "Abarth",
+        modello: "800",
+        alimentazione: "elettrico",
+    },
+    { 
+        marca: "Abarth",
+        modello: "900",
+        alimentazione: "metano",
+    },
+    { 
+        marca: "Abarth",
+        modello: "xxx",
+        alimentazione: "benzina",
+    },
+    { 
+        marca: "Abarth",
+        modello: "yyy",
+        alimentazione: "diesel",
+    },
+    { 
+        marca: "Abarth",
+        modello: "zzz",
+        alimentazione: "gpl",
+    },
+    { 
+        marca: "Abarth",
+        modello: "sss",
+        alimentazione: "elettrico",
+    },
+    { 
+        marca: "Abarth",
+        modello: "bigT",
+        alimentazione: "metano",
+    },
+    { 
+        marca: "Abarth",
+        modello: "family",
+        alimentazione: "benzina",
+    },
+    { 
+        marca: "Abarth",
+        modello: "turister",
+        alimentazione: "diesel",
+    },
+    { 
+        marca: "Abarth",
+        modello: "sport",
+        alimentazione: "gpl",
+    },
+    { 
+        marca: "Abarth",
+        modello: "offRoad",
+        alimentazione: "elettrico",
+    },
+    { 
+        marca: "Abarth",
+        modello: "Basic",
+        alimentazione: "metano",
+    }
+];
